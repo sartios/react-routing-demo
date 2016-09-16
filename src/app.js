c8c76e9509f8e55c0cf0b7d9128766fa3f6b2f52
U-SAVRAMIS\ssavr <ssavr@savramis.eurodyn.com>
2016-09-16 14:46:29 +0300

    add configuration

---
+++ src/app.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function(){
+    return (<div>
+      <h2>Hello World</h2>
+    </div>);
+  }
+});
+
+module.exports = App;
